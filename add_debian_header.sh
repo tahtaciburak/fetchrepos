a892292d944984f5369f169c3abe22e96b4dbe51
tahtaciburak <tahtaciburak@gmail.com>
2020-02-19 14:57:36 +0300

    Last changes

---
+++ add_debian_header.sh
@@ -0,0 +1,9 @@
+echo "Package: fetchrepos
+Version: $1
+Section: base
+Priority: optional
+Architecture: i386
+Maintainer: Burak TAHTACI <tahtaciburak@gmail.com>
+Description: fetchrepos
+ A tool for downloading repos from a user.
+">$2/control
